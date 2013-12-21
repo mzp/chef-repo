81aa8224dca9db95119793d4178932e3e12af558
mzp <mzpppp@gmail.com>
2013-12-21 00:45:26 +0000

    Fix home recipe: URL and path

--- cookbooks/home/recipes/default.rb
+++ cookbooks/home/recipes/default.rb
@@ -7,13 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 git "/home/mzp/home" do
-  repository "git@github.com:mzp/home.git"
+  repository "https://github.com/mzp/home"
   reference "master"
   action :sync
   user "mzp"
   group "mzp"
 end
-Dir["~mzp/home/dotfiles/*"].each do|file|
+Dir["/home/mzp/home/dotfiles/*"].each do|file|
   bash "install #{File.basename(file)}" do
     code <<-END
       rm -rf ~mzp/#{File.basename(file)}
