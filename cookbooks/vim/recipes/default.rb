9a6dedf3f23b299ed5aed87bbea0c06fcc68a457
mzp <mzpppp@gmail.com>
2013-12-21 03:07:05 +0000

    Fix vim recipe: install NeoBundle

--- cookbooks/vim/recipes/default.rb
+++ cookbooks/vim/recipes/default.rb
@@ -9,3 +9,29 @@
 package 'vim' do
   action :install
 end
+
+directory '/home/mzp/.vim' do
+  owner 'mzp'
+  group 'mzp'
+  mode '0755 '
+  action :create
+end
+
+directory '/home/mzp/.vim/bundle' do
+  owner 'mzp'
+  group 'mzp'
+  mode '0755 '
+  action :create
+end
+
+git "/home/mzp/.vim/bundle/neobundle.vim" do
+  repository "https://github.com/Shougo/neobundle.vim"
+  reference "master"
+  action :sync
+  user "mzp"
+  group "mzp"
+end
+
+bash "install NeoBundle" do
+  code "sudo -u mzp /home/mzp/.vim/bundle/neobundle.vim/bin/neoinstall"
+end
