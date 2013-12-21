c7ad8b6615b23846a9c6e10e380384b960e45eb9
mzp <mzpppp@gmail.com>
2013-12-21 13:21:54 +0000

    add recipe: rbenv

---
+++ cookbooks/rbenv/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: rbenv
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+git "/home/mzp/.rbenv" do
+  repository "https://github.com/sstephenson/rbenv.git"
+  reference "master"
+  action :sync
+  user "mzp"
+  group "mzp"
+end
+
+directory '/home/mzp/.rbenv/plugins' do
+  owner 'mzp'
+  group 'mzp'
+  mode '0755 '
+  action :create
+end
+
+git "/home/mzp/.rbenv/plugins/ruby-build" do
+  repository "https://github.com/sstephenson/ruby-build.git"
+  reference "master"
+  action :sync
+  user "mzp"
+  group "mzp"
+end
+
+directory '/home/mzp/profiles' do
+  owner 'mzp'
+  group 'mzp'
+  mode '0755 '
+  action :create
+end
+
+template "/home/mzp/profiles/rbenv.profile" do
+  source "rbenv.profile"
+  owner "mzp"
+  group "mzp"
+  mode 0644
+end
