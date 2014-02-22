997e186e72a8b4176584ce3564b70a92e896653c
root <root@28e0f1612d94.(none)>
2014-02-22 02:48:14 +0000

    add atig

---
+++ cookbooks/atig/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: atig
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+%w(ruby rubygems ruby-dev libsqlite3-dev ca-certificates).each do|name|
+  package name do
+    action :install
+  end
+end
+
+gem_package "atig" do
+  action :install
+end
