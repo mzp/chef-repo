f1e211080002fd387a3e9e313c3fe03e48ca32f7
mzp <mzpppp@gmail.com>
2014-04-25 20:39:44 +0900

    add recipe: proofgeneral

---
+++ cookbooks/proofgeneral/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: emacs
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'emacs24-nox' do
+  action :install
+end
+
+package 'proofgeneral' do
+  action :install
+end
