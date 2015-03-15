c26fa7e5d9d3250424c471f9180c1ba0b2293bca
mzp <mzpppp@gmail.com>
2015-03-15 12:07:08 +0900

    makedir

--- cookbooks/user/recipes/default.rb
+++ cookbooks/user/recipes/default.rb
@@ -15,6 +15,11 @@ bash 'make as sudoers' do
   code 'usermod -G mzp,sudo mzp'
 end
 
+bash 'make .ssh' do
+  code 'mkdir /home/mzp/.ssh'
+  code 'chown mzp:mzp /home/mzp/.ssh'
+end
+
 template '/home/mzp/.ssh/authorized_keys' do
   source 'authorized_keys'
   owner 'mzp'
